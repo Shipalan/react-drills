6c7df59b6d88814f062823641915fd6670319d4a
Shipalan <shipalan678@gmail.com>
2022-07-30 21:22:38 -0600

    Working on ToDoList

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import ToDoList from "./Components/ToDoList";
 
 function App() {
+  const [toDo, setToDo] = useState([]);
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Add a task!</h2>
+      <form>
+        <label>
+          <input
+            placeholder="Task here"
+            name="addTask"
+            onSubmit={(e) => setToDo(e.target.value)}
+          ></input>
+        </label>
+        <button>Submit</button>
+      </form>
+      <ToDoList item={toDo}></ToDoList>
     </div>
   );
 }
